9d1a22443e78435f9e9d61b6179d58d3d438e4eb
Benoit Kekwandi <benoitkekwandi@gmail.com>
2025-01-17 16:23:28 +0200

    API gateway update(downgraded and fixed)

--- api-gateway/src/test/java/com/telusko/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/telusko/apigateway/ApiGatewayApplicationTests.java
@@ -1,11 +1,9 @@
-package com.api_gateway;
+package com.telusko.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootTest
-@EnableDiscoveryClient
 class ApiGatewayApplicationTests {
 
 	@Test
